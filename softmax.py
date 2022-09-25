16c7614a9b8f6693f19681b915a7435787f7ef40
Julian Wandhoven <Julian.wandhoven@gmail.com>
2022-09-25 23:17:49 +0200

    25 sep

--- softmax.py
+++ softmax.py
@@ -2,10 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 nums = 500
-minx = -2
-maxx =  2
-miny = -2
-maxy =  2
+val = 1.5
+minx = -val
+maxx =  val
+miny = -val
+maxy =  val
 
 def softmax_x(x, y):
     return np.e**x / (np.e**x + np.e**y)
@@ -17,18 +18,21 @@ y = np.flip(np.linspace(miny, maxy, num=nums))
 
 x, y = np.meshgrid(x, y)
 plt.imshow(softmax_x(x, y), extent=[minx,maxx,miny,maxy], cmap=plt.cm.turbo)
-plt.ylabel("dimension 1")
-plt.xlabel("dimension 2")
+plt.ylabel(r"$v_{1}$")
+plt.xlabel(r"$v_{2}$")
 plt.savefig("images/softmaxx.svg", bbox_inches='tight', pad_inches=0)
 plt.clf()
 plt.imshow(softmax_y(x, y), extent=[minx,maxx,miny,maxy], cmap=plt.cm.turbo)
-plt.ylabel("dimension 1")
-plt.xlabel("dimension 2")
+plt.ylabel(r"$v_{1}$")
+plt.xlabel(r"$v_{2}$")
 plt.savefig("images/softmaxy.svg", bbox_inches='tight', pad_inches=0)
 plt.clf()
 plt.axes().get_xaxis().set_visible(False)
 plt.imshow(np.array([np.linspace(0, 1, num=nums)]).T, extent=[0, 1, -1, 1], cmap=plt.cm.turbo, aspect=20)
 plt.xlabel("scale")
 plt.savefig("images/softmaxscale.svg", bbox_inches='tight', pad_inches=0)
-plt.show()
+#plt.show()
 plt.clf()
+
+#plt.xlabel(r"$s_{a}$")
+#plt.ylabel(r"$r_{a}$")
