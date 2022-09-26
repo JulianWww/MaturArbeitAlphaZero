f5ec64f9b97f0953a8c2de047e436b6137ea423c
JulianWww <julian.wandhoven@gmail.com>
2022-09-26 11:38:38 +0200

    added code

---
+++ getcode.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git clone https://github.com/JulianWww/AlphaZero code
+git clone https://github.com/JulianWww/Matura-AlphaZero-demos demos
+git clone https://github.com/JulianWww/NeuralNetwork networks
